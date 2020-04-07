71b0b985c63febe1e315c4a257f9b8635d35a32e
Jessica Rodrigues <jessicarodridc@gmail.com>
2020-04-07 17:39:04 -0300

    no message

---
+++ src/main/java/br/com/sistemavendas/service/ClienteService.java
@@ -0,0 +1,4 @@
+package br.com.sistemavendas.service;
+
+public interface ClienteService {
+}
---
+++ src/test/java/br/com/sistemavendas/rest/controller/UsuarioControllerTests.java
@@ -0,0 +1,4 @@
+package br.com.sistemavendas.rest.controller;
+
+public class UsuarioControllerTests {
+}
