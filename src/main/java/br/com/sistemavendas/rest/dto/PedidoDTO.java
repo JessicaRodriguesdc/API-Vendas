5c78a53922d122f9468391f516102f0d5a47f1c7
Jessica Rodrigues <jessicarodridc@gmail.com>
2020-04-07 17:01:10 -0300

    no message

--- src/main/java/br/com/sistemavendas/rest/dto/PedidoDTO.java
+++ src/main/java/br/com/sistemavendas/rest/dto/PedidoDTO.java
@@ -13,13 +13,13 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PedidoDTO {
-    //@NotNull(message = "Informe o codigo do cliente")
+
     @NotNull(message = "{campo.codigo-cliente.obrigatorio}")
     private Integer cliente;
-    //@NotNull(message = "Campo Total do pedido e obrigatorio")
+
     @NotNull(message = "{campo.total-pedido.obrigatorio}")
     private BigDecimal total;
-    //@NotEmptyList(message = "Pedido nao pode ser realizado sem itens.")
+
     @NotEmptyList(message = "{campo.items-pedido.obrigatorio}")
     private List<ItemPedidoDTO> items;
 
