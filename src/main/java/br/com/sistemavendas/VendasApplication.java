d3583d39aed29afc55813c7a44999951ac3571b4
Jessica Rodrigues <jessicarodridc@gmail.com>
2020-04-10 20:17:11 -0300

    add maven

--- src/main/java/br/com/sistemavendas/VendasApplication.java
+++ src/main/java/br/com/sistemavendas/VendasApplication.java
@@ -1,24 +1,17 @@
 package br.com.sistemavendas;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.modelmapper.ModelMapper;
 
 
 @SpringBootApplication
 @RestController
+//@ComponentScan({"sistemavendas"})
 public class VendasApplication extends SpringBootServletInitializer {
-
-    @Bean
-    public ModelMapper modelMapper(){
-        return new ModelMapper();
-    }
 
     public static void main(String[] args) {
 
