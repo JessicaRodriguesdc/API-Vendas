d258f4baf34f220061ce5e1493c542ee9686a00c
Jessica Rodrigues <jessicarodridc@gmail.com>
2020-04-10 15:20:07 -0300

    add descricao do nome e descricao do projeto

--- src/main/java/br/com/sistemavendas/VendasApplication.java
+++ src/main/java/br/com/sistemavendas/VendasApplication.java
@@ -1,18 +1,32 @@
 package br.com.sistemavendas;
 
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.modelmapper.ModelMapper;
 
 
 @SpringBootApplication
+@RestController
 public class VendasApplication extends SpringBootServletInitializer {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 
     public static void main(String[] args) {
 
         SpringApplication.run(VendasApplication.class,args);
     }
 
+    @GetMapping("/")
+    public String getInitial() {
+        return "Servidor executando";
+    }
 }
