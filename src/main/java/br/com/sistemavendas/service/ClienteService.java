e0539754a8d93f5bbe4d195081a806df34be0c5a
Jessica Rodrigues <jessicarodridc@gmail.com>
2020-04-27 16:30:46 -0300

    criando controller de cliente

--- src/main/java/br/com/sistemavendas/service/ClienteService.java
+++ src/main/java/br/com/sistemavendas/service/ClienteService.java
@@ -1,4 +1,21 @@
 package br.com.sistemavendas.service;
 
+import br.com.sistemavendas.domain.entity.Cliente;
+import br.com.sistemavendas.rest.dto.ClienteDTO;
+import io.swagger.models.auth.In;
+import org.springframework.data.domain.Example;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface ClienteService {
+    Cliente salvar(Cliente cliente);
+
+    Cliente atualizar(Integer id,Cliente cliente);
+
+    Optional<Cliente> obterCliente(Integer id);
+
+    List<Cliente> listar(Example exampleCliente);
+
+    void deletar(Cliente cliente);
 }
