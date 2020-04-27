44c41fad29135437389672423d3b644cf7b90938
Jessica Rodrigues <jessicarodridc@gmail.com>
2020-04-27 16:30:09 -0300

    criando dto de cliente

--- src/main/java/br/com/sistemavendas/rest/dto/ClienteDTO.java
+++ src/main/java/br/com/sistemavendas/rest/dto/ClienteDTO.java
@@ -1,4 +1,22 @@
 package br.com.sistemavendas.rest.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ClienteDTO {
+
+    @NotEmpty(message = "{campo.nome.obrigatorio}")
+    private String nome;
+
+    @NotEmpty(message = "{campo.cpf.obrigatorio}")
+    @CPF(message = "{campo.cpf.invalido}")
+    private String cpf;
+
 }
