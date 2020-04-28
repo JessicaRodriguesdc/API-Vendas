4b9b3306ea862956a544a389011dacd6c0ca5783
Jessica Rodrigues <jessicarodridc@gmail.com>
2020-04-28 14:51:46 -0300

    criando test de obter cliente por id

--- src/main/java/br/com/sistemavendas/rest/dto/ClienteDTO.java
+++ src/main/java/br/com/sistemavendas/rest/dto/ClienteDTO.java
@@ -1,6 +1,7 @@
 package br.com.sistemavendas.rest.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.br.CPF;
@@ -8,6 +9,7 @@ import org.hibernate.validator.constraints.br.CPF;
 import javax.validation.constraints.NotEmpty;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class ClienteDTO {
